1bc13f7c563578d656a9b0cc6ad5339947d64212
matt <m.mofrad@livelinktechnology.net>
2018-08-27 01:30:39 +0100

    title spash

---
+++ main.go
@@ -0,0 +1,74 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"time"
+
+	"github.com/veandco/go-sdl2/sdl"
+	ttf "github.com/veandco/go-sdl2/ttf"
+)
+
+func main() {
+	if err := run(); err != nil {
+		fmt.Fprintf(os.Stderr, "%v", err)
+		os.Exit(2)
+	}
+}
+
+func run() error {
+	err := sdl.Init(sdl.INIT_EVERYTHING)
+	if err != nil {
+		return fmt.Errorf("could not init SDL: %v", err)
+	}
+	defer sdl.Quit()
+
+	if err := ttf.Init(); err != nil {
+		return fmt.Errorf("could not init TTF: %v", err)
+	}
+
+	w, r, err := sdl.CreateWindowAndRenderer(800, 600, sdl.WINDOW_SHOWN)
+	if err != nil {
+		return fmt.Errorf("could not create window %v", err)
+	}
+	defer w.Destroy()
+	_ = r
+
+	if err := drawTitle(r); err != nil {
+		return fmt.Errorf("Could not draw title: %v", err)
+	}
+
+	time.Sleep(5 * time.Second)
+	return nil
+}
+
+func drawTitle(r *sdl.Renderer) error {
+	r.Clear()
+
+	f, err := ttf.OpenFont("res/fonts/joystix.ttf", 20)
+	if err != nil {
+		return fmt.Errorf("could not open front: %v", err)
+	}
+	defer f.Close()
+
+	c := sdl.Color{R: 225, G: 100, B: 0, A: 225}
+	s, err := f.RenderUTF8Solid("Zibby Bird", c)
+	if err != nil {
+		return fmt.Errorf("could not render title: %v", err)
+	}
+	defer s.Free()
+
+	t, err := r.CreateTextureFromSurface(s)
+	if err != nil {
+		return fmt.Errorf("could not create texture: %v", err)
+	}
+	defer t.Destroy()
+
+	if err := r.Copy(t, nil, nil); err != nil {
+		return fmt.Errorf("could not copy texture: %v", err)
+	}
+
+	r.Present()
+
+	return nil
+}
